24c960248cc75a8a58d42cb0b666d00861828d1d
JinPyoK <111625932+JinPyoK@users.noreply.github.com>
2024-12-05 22:07:32 +0900

    .

--- lib/provider/auth/google_login.dart
+++ lib/provider/auth/google_login.dart
@@ -3,8 +3,9 @@ part of 'oauth_provider.dart';
 Future<String?> _signInWithGoogle() async {
   /// Trigger the authentication flow
   try {
-    final GoogleSignInAccount? googleUser =
-        await GoogleSignIn(serverClientId: webClientId).signIn();
+    final googleSignIn = GoogleSignIn(serverClientId: webClientId);
+
+    final googleUser = await googleSignIn.signIn();
 
     /// Obtain the auth details from the request
     final GoogleSignInAuthentication? googleAuth =
@@ -18,7 +19,7 @@ Future<String?> _signInWithGoogle() async {
     _accessToken = googleAuth.accessToken;
 
     return null;
-  } catch (_) {
+  } catch (e) {
     return "구글 로그인 도중 에러가 발생했습니다";
   }
 }
