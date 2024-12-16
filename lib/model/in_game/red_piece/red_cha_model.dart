2598d9fd2892931772fb104d08499d5876d97b8c
JinPyoK <111625932+JinPyoK@users.noreply.github.com>
2024-12-16 17:06:12 +0900

    red cha 길찾기

--- lib/model/in_game/red_piece/red_cha_model.dart
+++ lib/model/in_game/red_piece/red_cha_model.dart
@@ -1,5 +1,7 @@
+import 'package:samyeonchoga/model/in_game/piece_actionable_model.dart';
 import 'package:samyeonchoga/model/in_game/piece_base_model.dart';
 import 'package:samyeonchoga/model/in_game/piece_enum.dart';
+import 'package:samyeonchoga/provider/in_game/in_game_board_status.dart';
 import 'package:samyeonchoga/ui/common/widget/image_assets.dart';
 
 final class RedChaModel extends RedPieceBaseModel {
@@ -19,5 +21,154 @@ final class RedChaModel extends RedPieceBaseModel {
     pieceActionable.clear();
 
     /// 기물이 갈 수 있는 길을 찾아서 리스트에 넣는다.
+    void findRedChaActions(PieceOrJustActionable status) {
+      if (status is PieceActionableModel) {
+        pieceActionable.add(
+          PieceActionableModel(
+            targetX: status.targetX,
+            targetY: status.targetY,
+            targetValue: 0,
+          ),
+        );
+      } else if (status is PieceBaseModel) {
+        if (status.team == Team.blue) {
+          pieceActionable.add(
+            PieceActionableModel(
+              targetX: status.x,
+              targetY: status.y,
+              targetValue: status.value,
+            ),
+          );
+        }
+      }
+    }
+
+    /// 위
+    for (int i = y - 1; i >= 0; i--) {
+      final status = getStatus(x, i);
+      if (status is PieceBaseModel) {
+        if (status.team == Team.red) {
+          break;
+        } else if (status.team == Team.blue) {
+          findRedChaActions(status);
+          break;
+        }
+      } else {
+        findRedChaActions(status);
+      }
+    }
+
+    /// 아래
+    for (int i = y + 1; i < 10; i++) {
+      final status = getStatus(x, i);
+      if (status is PieceBaseModel) {
+        if (status.team == Team.red) {
+          break;
+        } else if (status.team == Team.blue) {
+          findRedChaActions(status);
+          break;
+        }
+      } else {
+        findRedChaActions(status);
+      }
+    }
+
+    /// 왼쪽
+    for (int i = x - 1; i >= 0; i--) {
+      final status = getStatus(i, y);
+      if (status is PieceBaseModel) {
+        if (status.team == Team.red) {
+          break;
+        } else if (status.team == Team.blue) {
+          findRedChaActions(status);
+          break;
+        }
+      } else {
+        findRedChaActions(status);
+      }
+    }
+
+    /// 오른쪽
+    for (int i = x + 1; i < 9; i++) {
+      final status = getStatus(i, y);
+      if (status is PieceBaseModel) {
+        if (status.team == Team.red) {
+          break;
+        } else if (status.team == Team.blue) {
+          findRedChaActions(status);
+          break;
+        }
+      } else {
+        findRedChaActions(status);
+      }
+    }
+
+    /// 차가 궁성 내부에 있을 때
+    if (x == 3 && y == 7) {
+      for (int i = x + 1, j = y + 1; i <= 5 && j <= 9; i++, j++) {
+        final status = getStatus(i, j);
+
+        if (status is PieceBaseModel) {
+          if (status.team == Team.red) {
+            break;
+          } else if (status.team == Team.blue) {
+            findRedChaActions(status);
+            break;
+          }
+        } else {
+          findRedChaActions(status);
+        }
+      }
+    } else if (x == 5 && y == 7) {
+      for (int i = x - 1, j = y + 1; i >= 3 && j <= 9; i--, j++) {
+        final status = getStatus(i, j);
+
+        if (status is PieceBaseModel) {
+          if (status.team == Team.red) {
+            break;
+          } else if (status.team == Team.blue) {
+            findRedChaActions(status);
+            break;
+          }
+        } else {
+          findRedChaActions(status);
+        }
+      }
+    } else if (x == 4 && y == 8) {
+      findRedChaActions(getStatus(3, 7));
+      findRedChaActions(getStatus(5, 7));
+      findRedChaActions(getStatus(3, 9));
+      findRedChaActions(getStatus(5, 9));
+    } else if (x == 3 && y == 9) {
+      for (int i = x + 1, j = y - 1; i <= 5 && j >= 7; i++, j--) {
+        final status = getStatus(i, j);
+
+        if (status is PieceBaseModel) {
+          if (status.team == Team.red) {
+            break;
+          } else if (status.team == Team.blue) {
+            findRedChaActions(status);
+            break;
+          }
+        } else {
+          findRedChaActions(status);
+        }
+      }
+    } else if (x == 5 && y == 9) {
+      for (int i = x - 1, j = y - 1; i >= 3 && j >= 7; i--, j--) {
+        final status = getStatus(i, j);
+
+        if (status is PieceBaseModel) {
+          if (status.team == Team.red) {
+            break;
+          } else if (status.team == Team.blue) {
+            findRedChaActions(status);
+            break;
+          }
+        } else {
+          findRedChaActions(status);
+        }
+      }
+    }
   }
 }
