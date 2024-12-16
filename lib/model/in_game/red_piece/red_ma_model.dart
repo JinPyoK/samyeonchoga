cf8533d184a566ec22c897268713c51833e9ce4a
JinPyoK <111625932+JinPyoK@users.noreply.github.com>
2024-12-16 19:39:13 +0900

    red ma 길찾기

--- lib/model/in_game/red_piece/red_ma_model.dart
+++ lib/model/in_game/red_piece/red_ma_model.dart
@@ -1,5 +1,7 @@
 import 'package:samyeonchoga/model/in_game/piece_base_model.dart';
 import 'package:samyeonchoga/model/in_game/piece_enum.dart';
+import 'package:samyeonchoga/model/in_game/red_piece/find_red_actions.dart';
+import 'package:samyeonchoga/provider/in_game/in_game_board_status.dart';
 import 'package:samyeonchoga/ui/common/widget/image_assets.dart';
 
 final class RedMaModel extends RedPieceBaseModel {
@@ -19,5 +21,107 @@ final class RedMaModel extends RedPieceBaseModel {
     pieceActionable.clear();
 
     /// 기물이 갈 수 있는 길을 찾아서 리스트에 넣는다.
+    /// 0: 아무것도 없음, 1: 한나라의 기물이 있음, 2: 초나라의 기물이 있음
+    List<dynamic> redMaStatusProcessing(int x, int y) {
+      final status = getStatus(x, y);
+      if (status is PieceBaseModel) {
+        if (status.team == Team.red) {
+          return [1, status];
+        } else {
+          return [2, status];
+        }
+      } else {
+        return [0, status];
+      }
+    }
+
+    /// 위
+    if (y >= 2) {
+      final proc = redMaStatusProcessing(x, y - 1);
+
+      /// 왼쪽
+      if (proc[0] == 0) {
+        if (x > 0) {
+          final proc = redMaStatusProcessing(x - 1, y - 2);
+          if (proc[0] != 1) {
+            findRedActions(proc[1], pieceActionable);
+          }
+        }
+
+        /// 오른쪽
+        if (x < 8) {
+          final proc = redMaStatusProcessing(x + 1, y - 2);
+          if (proc[0] != 1) {
+            findRedActions(proc[1], pieceActionable);
+          }
+        }
+      }
+    }
+
+    /// 아래
+    if (y <= 7) {
+      final proc = redMaStatusProcessing(x, y + 1);
+      if (proc[0] == 0) {
+        /// 왼쪽
+        if (x > 0) {
+          final proc = redMaStatusProcessing(x - 1, y + 2);
+          if (proc[0] != 1) {
+            findRedActions(proc[1], pieceActionable);
+          }
+        }
+
+        /// 오른쪽
+        if (x < 8) {
+          final proc = redMaStatusProcessing(x + 1, y + 2);
+          if (proc[0] != 1) {
+            findRedActions(proc[1], pieceActionable);
+          }
+        }
+      }
+    }
+
+    /// 왼쪽
+    if (x >= 2) {
+      final proc = redMaStatusProcessing(x - 1, y);
+      if (proc[0] == 0) {
+        /// 위
+        if (y > 0) {
+          final proc = redMaStatusProcessing(x - 2, y - 1);
+          if (proc[0] != 1) {
+            findRedActions(proc[1], pieceActionable);
+          }
+        }
+
+        /// 아래
+        if (y < 9) {
+          final proc = redMaStatusProcessing(x - 2, y + 1);
+          if (proc[0] != 1) {
+            findRedActions(proc[1], pieceActionable);
+          }
+        }
+      }
+    }
+
+    /// 오른쪽
+    if (x <= 6) {
+      final proc = redMaStatusProcessing(x + 1, y);
+      if (proc[0] == 0) {
+        /// 위
+        if (y > 0) {
+          final proc = redMaStatusProcessing(x + 2, y - 1);
+          if (proc[0] != 1) {
+            findRedActions(proc[1], pieceActionable);
+          }
+        }
+
+        /// 아래
+        if (y < 9) {
+          final proc = redMaStatusProcessing(x + 2, y + 1);
+          if (proc[0] != 1) {
+            findRedActions(proc[1], pieceActionable);
+          }
+        }
+      }
+    }
   }
 }
