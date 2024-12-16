07b6cdd2b879b38e7e8efcd9d83c0874fc1f4cc9
JinPyoK <111625932+JinPyoK@users.noreply.github.com>
2024-12-16 20:31:04 +0900

    blue po, ma, sang 길찾기

--- lib/model/in_game/blue_piece/blue_ma_model.dart
+++ lib/model/in_game/blue_piece/blue_ma_model.dart
@@ -1,5 +1,7 @@
+import 'package:samyeonchoga/model/in_game/blue_piece/find_blue_actions.dart';
 import 'package:samyeonchoga/model/in_game/piece_base_model.dart';
 import 'package:samyeonchoga/model/in_game/piece_enum.dart';
+import 'package:samyeonchoga/provider/in_game/in_game_board_status.dart';
 import 'package:samyeonchoga/ui/common/widget/image_assets.dart';
 
 final class BlueMaModel extends BluePieceBaseModel {
@@ -19,5 +21,118 @@ final class BlueMaModel extends BluePieceBaseModel {
     pieceActionable.clear();
 
     /// 기물이 갈 수 있는 길을 찾아서 리스트에 넣는다.
+    /// 0: 아무것도 없음, 1: 한나라의 기물이 있음, 2: 초나라의 기물이 있음
+    List<dynamic> blueMaStatusProcessing(int x, int y) {
+      final status = getStatus(x, y);
+      if (status is PieceBaseModel) {
+        if (status.team == Team.blue) {
+          return [1, status];
+        } else {
+          return [2, status];
+        }
+      } else {
+        return [0, status];
+      }
+    }
+
+    /// 위
+    if (y >= 2) {
+      final proc = blueMaStatusProcessing(x, y - 1);
+
+      if (proc[0] == 0) {
+        /// 왼쪽
+        if (x > 0) {
+          final proc = blueMaStatusProcessing(x - 1, y - 2);
+
+          if (proc[0] != 1) {
+            findBlueActions(proc[1], pieceActionable);
+          }
+        }
+
+        /// 오른쪽
+        if (x < 8) {
+          final proc = blueMaStatusProcessing(x + 1, y - 2);
+
+          if (proc[0] != 1) {
+            findBlueActions(proc[1], pieceActionable);
+          }
+        }
+      }
+    }
+
+    /// 아래
+    if (y <= 7) {
+      final proc = blueMaStatusProcessing(x, y + 1);
+
+      if (proc[0] == 0) {
+        /// 왼쪽
+        if (x > 0) {
+          final proc = blueMaStatusProcessing(x - 1, y + 2);
+
+          if (proc[0] != 1) {
+            findBlueActions(proc[1], pieceActionable);
+          }
+        }
+
+        /// 오른쪽
+        if (x < 8) {
+          final proc = blueMaStatusProcessing(x + 1, y + 2);
+
+          if (proc[0] != 1) {
+            findBlueActions(proc[1], pieceActionable);
+          }
+        }
+      }
+    }
+
+    /// 왼쪽
+    if (x >= 2) {
+      final proc = blueMaStatusProcessing(x - 1, y);
+
+      if (proc[0] == 0) {
+        /// 위
+        if (y > 0) {
+          final proc = blueMaStatusProcessing(x - 2, y - 1);
+
+          if (proc[0] != 1) {
+            findBlueActions(proc[1], pieceActionable);
+          }
+        }
+
+        /// 아래
+        if (y < 9) {
+          final proc = blueMaStatusProcessing(x - 2, y + 1);
+
+          if (proc[0] != 1) {
+            findBlueActions(proc[1], pieceActionable);
+          }
+        }
+      }
+    }
+
+    /// 오른쪽
+    if (x <= 6) {
+      final proc = blueMaStatusProcessing(x + 1, y);
+
+      if (proc[0] == 0) {
+        /// 위
+        if (y > 0) {
+          final proc = blueMaStatusProcessing(x + 2, y - 1);
+
+          if (proc[0] != 1) {
+            findBlueActions(proc[1], pieceActionable);
+          }
+        }
+
+        /// 아래
+        if (y < 9) {
+          final proc = blueMaStatusProcessing(x + 2, y + 1);
+
+          if (proc[0] != 1) {
+            findBlueActions(proc[1], pieceActionable);
+          }
+        }
+      }
+    }
   }
 }
--- lib/model/in_game/blue_piece/blue_po_model.dart
+++ lib/model/in_game/blue_piece/blue_po_model.dart
@@ -1,5 +1,7 @@
+import 'package:samyeonchoga/model/in_game/blue_piece/find_blue_actions.dart';
 import 'package:samyeonchoga/model/in_game/piece_base_model.dart';
 import 'package:samyeonchoga/model/in_game/piece_enum.dart';
+import 'package:samyeonchoga/provider/in_game/in_game_board_status.dart';
 import 'package:samyeonchoga/ui/common/widget/image_assets.dart';
 
 final class BluePoModel extends BluePieceBaseModel {
@@ -18,6 +20,98 @@ final class BluePoModel extends BluePieceBaseModel {
     /// 현재 액션 가능한 리스트를 비워준다.
     pieceActionable.clear();
 
+    /// 포의 다리
+    bool bridge = false;
+
     /// 기물이 갈 수 있는 길을 찾아서 리스트에 넣는다.
+    /// for문 break를 원할 시 true 반환
+    bool bluePoStatusProcessing(int x, int y) {
+      final status = getStatus(x, y);
+
+      if (bridge) {
+        if (status is PieceBaseModel) {
+          if (status.team == Team.blue) {
+            return true;
+          } else {
+            if (status.pieceType == PieceType.po) {
+              return true;
+            } else {
+              findBlueActions(status, pieceActionable);
+              return true;
+            }
+          }
+        } else {
+          findBlueActions(status, pieceActionable);
+          return false;
+        }
+      } else {
+        if (status is PieceBaseModel) {
+          if (status.pieceType == PieceType.po) {
+            return true;
+          } else {
+            bridge = true;
+            return false;
+          }
+        } else {
+          return false;
+        }
+      }
+    }
+
+    bridge = false;
+
+    /// 위
+    for (int i = y - 1; i >= 0; i--) {
+      final breakNow = bluePoStatusProcessing(x, i);
+      if (breakNow) break;
+    }
+
+    bridge = false;
+
+    /// 아래
+    for (int i = y + 1; i <= 9; i++) {
+      final breakNow = bluePoStatusProcessing(x, i);
+      if (breakNow) break;
+    }
+
+    bridge = false;
+
+    /// 왼쪽
+    for (int i = x - 1; i >= 0; i--) {
+      final breakNow = bluePoStatusProcessing(i, y);
+      if (breakNow) break;
+    }
+
+    bridge = false;
+
+    /// 오른쪽
+    for (int i = x + 1; i <= 8; i++) {
+      final breakNow = bluePoStatusProcessing(i, y);
+      if (breakNow) break;
+    }
+    bridge = false;
+
+    /// 포가 궁성 내부에 있을 때
+    if (x == 3 && y == 7) {
+      for (int i = x + 1, j = y + 1; i <= 5 && j <= 9; i++, j++) {
+        final breakNow = bluePoStatusProcessing(i, j);
+        if (breakNow) break;
+      }
+    } else if (x == 5 && y == 7) {
+      for (int i = x - 1, j = y + 1; i >= 3 && j <= 9; i--, j++) {
+        final breakNow = bluePoStatusProcessing(i, j);
+        if (breakNow) break;
+      }
+    } else if (x == 3 && y == 9) {
+      for (int i = x + 1, j = y - 1; i <= 5 && j >= 7; i++, j--) {
+        final breakNow = bluePoStatusProcessing(i, j);
+        if (breakNow) break;
+      }
+    } else if (x == 5 && y == 9) {
+      for (int i = x - 1, j = y - 1; i >= 3 && j >= 7; i--, j--) {
+        final breakNow = bluePoStatusProcessing(i, j);
+        if (breakNow) break;
+      }
+    }
   }
 }
