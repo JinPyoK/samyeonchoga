22e69ccca424977e42857f2f19e9a9f996f63bd4
JinPyoK <111625932+JinPyoK@users.noreply.github.com>
2025-01-25 17:28:10 +0900

    메인 스크린에서 뒤로가기 누를 시 다이얼로그 보여주기

--- lib/ui/common/screen/home_navigation_screen.dart
+++ lib/ui/common/screen/home_navigation_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:samyeonchoga/core/constant/color.dart';
 import 'package:samyeonchoga/provider/context/global_context.dart';
 import 'package:samyeonchoga/provider/gold/gold_entity.dart';
 import 'package:samyeonchoga/ui/ad/screen/ad_screen.dart';
 import 'package:samyeonchoga/ui/common/controller/compare_store_version.dart';
+import 'package:samyeonchoga/ui/common/controller/show_custom_dialog.dart';
 import 'package:samyeonchoga/ui/common/controller/util_function.dart';
 import 'package:samyeonchoga/ui/common/widget/gold_widget.dart';
 import 'package:samyeonchoga/ui/home/screen/home_screen.dart';
@@ -40,62 +42,100 @@ class _HomeNavigationScreenState extends State<HomeNavigationScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: whiteColor,
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: GoldWidget(gold: myGold.gold),
-          ),
-        ],
-      ),
-      body: PageView(
-        controller: _pageController,
-        physics: const NeverScrollableScrollPhysics(),
-        children: const [
-          HomeScreen(),
-          RankScreen(),
-          AdScreen(),
-        ],
-      ),
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: _currentIndex,
-        onDestinationSelected: (index) {
-          _currentIndex = index;
-          _pageController.animateToPage(
-            index,
-            duration: const Duration(milliseconds: 300),
-            curve: Curves.easeOut,
-          );
-          setState(() {});
-        },
-        destinations: const <NavigationDestination>[
-          NavigationDestination(
-            icon: FaIcon(FontAwesomeIcons.houseChimney),
-            selectedIcon: FaIcon(
-              FontAwesomeIcons.houseChimney,
-              color: whiteColor,
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (_, __) {
+        showCustomDialog(
+            context,
+            defaultAction: false,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text("게임을 종료하시겠습니까?"),
+                const SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey,
+                      ),
+                      onPressed: () {
+                        Navigator.of(context, rootNavigator: true).pop();
+                      },
+                      child: const Text("취소"),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: redColor,
+                      ),
+                      onPressed: () {
+                        SystemNavigator.pop();
+                      },
+                      child: const Text("게임 종료"),
+                    )
+                  ],
+                )
+              ],
+            ));
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: whiteColor,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: GoldWidget(gold: myGold.gold),
             ),
-            label: "Home",
-          ),
-          NavigationDestination(
-            icon: FaIcon(FontAwesomeIcons.rankingStar),
-            selectedIcon: FaIcon(
-              FontAwesomeIcons.rankingStar,
-              color: whiteColor,
+          ],
+        ),
+        body: PageView(
+          controller: _pageController,
+          physics: const NeverScrollableScrollPhysics(),
+          children: const [
+            HomeScreen(),
+            RankScreen(),
+            AdScreen(),
+          ],
+        ),
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: _currentIndex,
+          onDestinationSelected: (index) {
+            _currentIndex = index;
+            _pageController.animateToPage(
+              index,
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeOut,
+            );
+            setState(() {});
+          },
+          destinations: const <NavigationDestination>[
+            NavigationDestination(
+              icon: FaIcon(FontAwesomeIcons.houseChimney),
+              selectedIcon: FaIcon(
+                FontAwesomeIcons.houseChimney,
+                color: whiteColor,
+              ),
+              label: "Home",
             ),
-            label: "Rank",
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.live_tv_outlined),
-            selectedIcon: Icon(
-              Icons.live_tv_outlined,
-              color: whiteColor,
+            NavigationDestination(
+              icon: FaIcon(FontAwesomeIcons.rankingStar),
+              selectedIcon: FaIcon(
+                FontAwesomeIcons.rankingStar,
+                color: whiteColor,
+              ),
+              label: "Rank",
             ),
-            label: "Ad",
-          ),
-        ],
+            NavigationDestination(
+              icon: Icon(Icons.live_tv_outlined),
+              selectedIcon: Icon(
+                Icons.live_tv_outlined,
+                color: whiteColor,
+              ),
+              label: "Ad",
+            ),
+          ],
+        ),
       ),
     );
   }
